b7ece2281156ffa6437bda7c7083d581e0299142
mikroelek <bogdanedem@gmail.com>
2021-01-14 23:05:00 +0100

    Coding more header and cpp files

---
+++ codes/Warrior.hpp
@@ -0,0 +1,20 @@
+#ifndef WARRIOR_HPP
+#define WARRIOR_HPP
+
+#include <string>
+
+class Warrior {
+    public:
+        void inputFromKeyboard();
+        std::string toString() const;
+        void attack(Warrior& defender) const;
+        bool isAlive() const;
+    private:
+        std::string name;
+        int health_points;
+        int damage;
+        int defense;
+        void die();
+};
+
+#endif
--- codes/main.cpp
+++ codes/main.cpp
@@ -1,29 +1,14 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-void printStatus(string& name1, int hp1, int dmg1, string& name2, int hp2, int dmg2){
-    cout <<name1 << "HP: " << hp1 << " DMG: " <<dmg1<<"  --- "<<name2<<  " HP: " <<hp2<<" DMG: "<<dmg2<<endl;;
-}
-
-void attack(string& attacker_name, int& attacker_hp, int& attacker_dmg, string& defender_name, int& defender_hp, int& defender_dmg){
-    printStatus(attacker_name,attacker_hp,attacker_dmg,defender_name,defender_hp,defender_dmg);
-    defender_hp-=attacker_dmg; 
-    if (defender_hp<0) defender_hp=0;
-}
+#include "Warrior.hpp"
+#include "Battle.hpp"
 
 int main() {
-    int hp1, dmg1, hp2, dmg2;
-    string name1, name2;
-    cin >>name1>> hp1 >>dmg1 >>name2>>hp2>>dmg2;
-
-    while (hp1>0 && hp2>0) {
-        attack(name1, hp1, dmg1, name2, hp2, dmg2);
-        if (hp2>0) { 
-            attack(name2, hp2, dmg2, name1, hp1, dmg1);
-        }
-    }
-printStatus(name1, hp1, dmg1, name2, hp2, dmg2);
+    Warrior warrior1, warrior2;
+    warrior1.inputFromKeyboard();
+    warrior2.inputFromKeyboard();
+    fighting(warrior1, warrior2);
 
 return 0;
 }
