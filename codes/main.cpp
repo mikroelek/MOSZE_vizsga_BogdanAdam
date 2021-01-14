fc8cb0b0aa325551dbba7da742a8e5ccd9afb6c4
mikroelek <bogdanedem@gmail.com>
2021-01-14 20:40:18 +0100

    Create fight instead of hello world

--- codes/main.cpp
+++ codes/main.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
-    cout<<"Hello World!\n";
+    int hp1, dmg1, hp2, dmg2;
+    cin >> hp1 >>dmg1 >>hp2>>dmg2;
+
+    while (hp1>0 && hp2>0) {
+        cout << "HP: " << hp1 << " DMG: " <<dmg1<<"  --->  HP: " <<hp2<<" DMG: "<<dmg2<<'\n';
+        hp2-=dmg1;
+        if (hp2<0) hp2=0; 
+        cout << "HP: " << hp1 << " DMG: " <<dmg1<<"  <---  HP: " <<hp2<<" DMG: "<<dmg2<<'\n';
+        hp1-=dmg2;
+        if (hp1<0) hp1=0;
+    }
+     cout << "HP: " << hp1 << " DMG: " <<dmg1<<"  ---  HP: " <<hp2<<" DMG: "<<dmg2<<'\n';
+
     return 0;
 }
