43841285716124bea2e6f34c8c5b869998c90f91
mikroelek <bogdanedem@gmail.com>
2021-01-19 19:45:25 +0100

    Fix missin main()

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -1,7 +1,14 @@
 #include "../Battle.hpp"
 #include <gtest/gtest.h>
 
+
 TEST(Tests, warriortest) {
     expected=2;
     EXPECT_EQ(expected, 2);
+}
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
